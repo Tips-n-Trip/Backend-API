f869a36a2a743271284b2012b42f28c8c6fb637e
Muhammad Imam Hidayat <imamhidayat24@gmail.com>
2023-06-02 14:11:27 +0700

    fix: middleware method for authenticate token

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,26 @@
-module.exports.profile = (req, res) => {
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
 
+module.exports.profile = async (req, res) => {
+  try {
+    const {id} = req.token;
+    const {name, email, iteneraries} = await prisma.user.findUnique(
+        {
+          where: {
+            id: id,
+          }, include: {
+            iteneraries: true,
+          },
+        },
+    );
+    const total_iteneraries = iteneraries.length;
+    const user = {name, email, total_iteneraries};
+    return res.status(200).json({
+      'success': true,
+      'message': 'data has found',
+      user,
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 module.exports.generateToken = (req, res, next) => {
-  return jwt.sign(req.id, process.env.ACCESS_TOKEN_SECRET);
+  return jwt.sign(req, process.env.ACCESS_TOKEN_SECRET);
 };
 
 module.exports.authenticateToken = (req, res, next) => {
-  const authHeader = req.header['Authorization'];
+  const authHeader = req.get('authorization');
   if (authHeader === null) {
     return res.status(401).json({
       'success': false,
@@ -19,14 +19,14 @@ module.exports.authenticateToken = (req, res, next) => {
       'message': 'Invalid authorization',
     });
   }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, token) => {
     if (error) {
       return res.status(403).json({
         'success': false,
         'message': 'Invalid JWT Token',
       });
     };
-    req.user = user;
+    req.token = token;
     next();
   });
 };
