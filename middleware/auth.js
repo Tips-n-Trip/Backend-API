08a6579166bbaa93a8f9a1dcc83eb72a2094ce89
Muhammad Imam Hidayat <imamhidayat24@gmail.com>
2023-06-09 14:26:15 +0700

    fix(middleware): updated middleware for user authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -21,14 +21,14 @@ module.exports.authenticateToken = (req, res, next) => {
       'message': 'Invalid authorization',
     });
   }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, id) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, token) => {
     if (error) {
       return res.status(403).json({
         'success': false,
         'message': 'Invalid JWT Token',
       });
     };
-    req.id = id;
+    req.token = token;
     next();
   });
 };
